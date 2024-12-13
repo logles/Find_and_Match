a3989a0c09bcb4d9dd4c2951f9b8008e89d3f6bc
logles <lydialogles@gmail.com>
2024-12-12 21:36:56 -0700

    responsive cards to chosen difficulty

--- js/pageElements.js
+++ js/pageElements.js
@@ -4,6 +4,18 @@ const easyButton = document.querySelector("#easy");
 const mediumButton = document.querySelector("#medium");
 const hardButtom = document.querySelector("#hard");
 const difficultyMenu = document.querySelector("#difficultyMenu");
+const easyCards = document.querySelectorAll(
+  "#card1, #card2, #card3, #card4, #card5, #card6, #card7, #card8"
+);
+const mediumCards = document.querySelectorAll(
+  "#card9, #card10, #card11, #card12"
+);
+const hardCards = document.querySelectorAll(
+  "#card13, #card14, #card14, #card16, #card17, #card18"
+);
+console.log(easyCards);
+console.log(mediumCards);
+console.log(hardCards);
 
 let chosenDifficulty = "";
 
@@ -17,12 +29,39 @@ document
 
       if (difficulty === "Easy") {
         chosenDifficulty = "Easy";
+        easyCards.forEach(function (card) {
+          card.style.display = "block";
+        });
+        mediumCards.forEach(function (card) {
+          card.style.display = "none";
+        });
+        hardCards.forEach(function (card) {
+          card.style.display = "none";
+        });
       }
       if (difficulty === "Medium") {
         chosenDifficulty = "Medium";
+        easyCards.forEach(function (card) {
+          card.style.display = "block";
+        });
+        mediumCards.forEach(function (card) {
+          card.style.display = "block";
+        });
+        hardCards.forEach(function (card) {
+          card.style.display = "none";
+        });
       }
       if (difficulty === "Hard") {
         chosenDifficulty = "Hard";
+        easyCards.forEach(function (card) {
+          card.style.display = "block";
+        });
+        mediumCards.forEach(function (card) {
+          card.style.display = "block";
+        });
+        hardCards.forEach(function (card) {
+          card.style.display = "block";
+        });
       }
     }
 
